84c46d03c9d25e97f00fda1a051245852498ceb6
Arian Fornaris <developers@phasereditor2d.com>
2020-04-08 11:27:03 -0400

    Convert to webp script.

---
+++ convertimages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for name in `ls images/*.png`; do
+
+    new_name=images/$(basename $name .png).webp
+    echo $new_name
+    convert $name -define webp:lossless=true $new_name
+    mv $name .backup-images/
+done
